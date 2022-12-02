b84009c5051a66813232cf5d9333793cc2b3854a
Sebas0172 <104285066+Sebas0172@users.noreply.github.com>
2022-12-01 20:56:29 -0600

    pruebas

--- frontend/setup.py
+++ frontend/setup.py
@@ -9,7 +9,7 @@ from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-
+#prueba 2
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
